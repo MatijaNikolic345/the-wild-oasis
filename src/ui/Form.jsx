195e108a75ea64fabef46af94b66074b6de8e168
MatijaNikk <matijasvi@gmail.com>
2024-02-01 17:01:33 +0100

    Final commit

--- src/ui/Form.jsx
+++ src/ui/Form.jsx
@@ -2,7 +2,7 @@ import styled, { css } from "styled-components";
 
 const Form = styled.form`
   width: 80rem;
-  overflow: hidden;
+
   font-size: 1.4rem;
   display: flex;
   flex-direction: column;
@@ -22,7 +22,7 @@ const Form = styled.form`
     props.type === "regular" &&
     css`
       /* Box */
-      width: 120rem;
+      width: 100%;
       background-color: var(--color-grey-0);
       border: 1px solid var(--color-grey-100);
       border-radius: var(--border-radius-md);
